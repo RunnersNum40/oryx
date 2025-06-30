959ba9c0018b4a609b5c018028d3bb296d7a1ff8
Ted <ted.pinkerton@mail.utoronto.ca>
2025-06-30 19:39:04 -0400

    Start environment

---
+++ oryx/env/base_env.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+
+import equinox as eqx
+from jaxtyping import Array, Bool, Float, Key
+
+from oryx.spaces.space import AbstractSpace
+
+
+class AbstractEnv[ActType, ObsType](eqx.Module, strict=True):
+    """Base class for RL environments"""
+
+    state_index: eqx.AbstractVar[eqx.nn.StateIndex]
+
+    @abstractmethod
+    def reset(self, state: eqx.nn.State, *, key: Key | None) -> eqx.nn.State:
+        """Reset the environment to an initial state"""
+
+    @abstractmethod
+    def step(
+        self, state: eqx.nn.State, action: ActType, *, key: Key | None
+    ) -> tuple[
+        eqx.nn.State, ObsType, Float[Array, ""], Bool[Array, ""], Bool[Array, ""], dict
+    ]:
+        """
+        Perform a step of the environment
+
+        :param state: The environment state
+        :param action: The action
+        :param key: Random key
+        :returns: The new environment state, the observation, the reward, termination
+            flag, truncation flag, and info dict
+        """
+
+    @abstractmethod
+    def render(self, state: eqx.nn.State):
+        """Render a frame from a state"""
+
+    def close(self):
+        """Close the environment"""
+        pass
+
+    @property
+    @abstractmethod
+    def action_space(self) -> AbstractSpace[ActType]:
+        """Return the action space of the environment"""
+
+    @property
+    @abstractmethod
+    def observation_space(self) -> AbstractSpace[ObsType]:
+        """Return the observation space of the environment"""
+
+    @property
+    def unwrapped(self) -> AbstractEnv[ActType, ObsType]:
+        """Return the unwrapped environment"""
+        return self
