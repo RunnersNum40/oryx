9bf99affc827bab8ecca4ade30431eea4b28610f
Ted <p_t@fastmail.net>
2025-07-22 14:18:41 -0400

    Move spaces/space to spaces/base_space

--- oryx/spaces/__init__.py
+++ oryx/spaces/__init__.py
@@ -4,6 +4,6 @@ Oryx spaces
 AbstractSpace, Box, Dict, Discrete, MultiDiscrete, Tuple
 """
 
-from .space import AbstractSpace, Box, Dict, Discrete, MultiDiscrete, Tuple
+from .base_space import AbstractSpace, Box, Dict, Discrete, MultiDiscrete, Tuple
 
 __all__ = ["AbstractSpace", "Box", "Dict", "Discrete", "MultiDiscrete", "Tuple"]
